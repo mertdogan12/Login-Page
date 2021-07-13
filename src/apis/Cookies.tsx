bc09415c2f11e56a910564591a1011ce397bd52d
Mert Dogan <mert.dogan@me.com>
2021-07-13 16:52:12 +0200

    Cookies.tsx createt

---
+++ src/apis/Cookies.tsx
@@ -0,0 +1,23 @@
+export function SetCookie(name: string, value: string, exdays: number) {
+  const date: Date = new Date();
+  date.setTime(date.getTime() + exdays * 24 * 60 * 60 * 1000);
+
+  let expires = "expires=" + date.toUTCString();
+  document.cookie = `${name}=${value};${expires};path=/`;
+}
+
+export function GetCookie(name: string): string {
+  let cookieString: string = decodeURIComponent(document.cookie);
+  let cookies: string[] = cookieString.includes(";")
+    ? cookieString.split(";")
+    : [cookieString];
+
+  for (let i = 0; i < cookies.length; i++) {
+    const cookie: string = cookies[i];
+    let values: string[] = cookie.split("=");
+
+    if (values[0] === name) return values[1];
+  }
+
+  return "";
+}

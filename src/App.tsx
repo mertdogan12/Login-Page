d35921cf7bc90bf909363a6a7a65df5f147150cf
Mert Dogan <mert.dogan@me.com>
2021-07-18 03:14:57 +0200

    error as get
    not working cant get the get params in the url

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,19 @@ import {
   Switch,
   Route,
   Redirect,
+  useLocation,
 } from "react-router-dom";
 import Login from "./sides/Login";
 import NavBar from "./sides/NavBar";
 import Settings from "./sides/Settings";
+import Error from "./sides/Error";
 
 function App() {
+  let location = useLocation();
+
   return (
     <Router>
+      <Error error={parseParams("error", "")} />
       <Switch>
         <Route exact path="/">
           <Redirect to="/dashboard/start" />
@@ -47,6 +52,22 @@ function App() {
       </Switch>
     </Router>
   );
+}
+
+function parseParams(name: string, getString: string): string {
+  if (!getString) return "";
+
+  getString = getString.replace("?", "");
+  const values: string[] = getString.includes("&")
+    ? getString.split("&")
+    : [getString];
+
+  for (let i = 0; i < values.length; i++) {
+    const element: string[] = values[i].split("=");
+    if (element[0] === name) return element[1];
+  }
+
+  return "";
 }
 
 export default App;
--- src/sides/Login.tsx
+++ src/sides/Login.tsx
@@ -42,8 +42,9 @@ class Login extends React.Component<MyProps, MyState> {
 
       this.props.history.push("/dashboard/start");
     } catch (error: any) {
-      this.setState({
-        error: error.name,
+      this.props.history.push({
+        pathname: "/auth/login",
+        search: `?error=${error.name}`,
       });
 
       return;
@@ -62,29 +63,26 @@ class Login extends React.Component<MyProps, MyState> {
 
   render() {
     return (
-      <div id="outside">
-        <Error error={this.state.error} />
-        <div id="login" onKeyDown={this.onKeyDown}>
-          <input
-            className="loginInput"
-            id="username"
-            placeholder="Username"
-            type="text"
-          />
-          <input
-            className="loginInput"
-            id="password"
-            placeholder="Password"
-            type="password"
-          />
-          <button className="loginButton" onClick={this.onLoginClick}>
-            Login
-          </button>
-          <div id="seperator">OR</div>
-          <button className="loginButton" id="register">
-            Register
-          </button>
-        </div>
+      <div id="login" onKeyDown={this.onKeyDown}>
+        <input
+          className="loginInput"
+          id="username"
+          placeholder="Username"
+          type="text"
+        />
+        <input
+          className="loginInput"
+          id="password"
+          placeholder="Password"
+          type="password"
+        />
+        <button className="loginButton" onClick={this.onLoginClick}>
+          Login
+        </button>
+        <div id="seperator">OR</div>
+        <button className="loginButton" id="register">
+          Register
+        </button>
       </div>
     );
   }

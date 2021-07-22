75a61fffed7b779d4455dc19f8909893440b3c7d
Mert Dogan <mert.dogan@me.com>
2021-07-23 01:42:32 +0200

    alert fix from changeUsername
    alert pops up even when the alert message not changes

--- src/sides/usersettings/ChangeUsername.tsx
+++ src/sides/usersettings/ChangeUsername.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, KeyboardEvent } from "react";
 import Alert from "../Alert";
 import { changeUsername } from "../../apis/authServer/Users";
 import { GetCookie } from "../../apis/Cookies";
@@ -6,6 +6,7 @@ import { GetCookie } from "../../apis/Cookies";
 function ChangeUsername() {
   let [alert, setAlert] = useState("");
   let [color, setColor] = useState("255;0;0");
+  let [count, setCount] = useState(1);
 
   async function onButtonClick() {
     const newUsername: HTMLInputElement = document.getElementById(
@@ -13,21 +14,35 @@ function ChangeUsername() {
     ) as HTMLInputElement;
 
     if (!newUsername.value) {
+      const errorMessage: string = "New Username is Empty";
       setColor("255;0;0");
-      setAlert("New Username is Empty");
-      console.log("Error");
+
+      if (alert === errorMessage) {
+        setCount(count + 1);
+      } else setAlert(errorMessage);
+
       return;
     }
 
     try {
+      const alertMessage: string = "Username has been changed";
       await changeUsername(newUsername.value, GetCookie("jwttoken"));
-
       setColor("179;146;0;0");
-      setAlert("Username has been changed");
+
+      if (alert === alertMessage) {
+        setCount(count + 1);
+      } else setAlert("Username has been changed");
     } catch (error) {
       setColor("255;0;0");
-      setAlert(error.name);
+
+      if (alert === error.name) {
+        setCount(count + 1);
+      } else setAlert(error.name);
     }
+  }
+
+  function onKeyDown(event: KeyboardEvent<HTMLInputElement>) {
+    if (event.key === "Enter") onButtonClick();
   }
 
   return (
@@ -35,7 +50,7 @@ function ChangeUsername() {
       <h2 className="header">Change Username</h2>
       <Alert id="changeUsernameAlert" color={color} alert={alert} />
       <label className="settingsLabel">New Username</label>
-      <input className="settingsInput" id="newUsername" />
+      <input className="settingsInput" onKeyDown={onKeyDown} id="newUsername" />
       <br />
       <button className="settingsButtons" onClick={onButtonClick}>
         Change Username

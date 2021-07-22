bc21c7017d1c960b99ebd62ae030307084f5636f
Mert Dogan <mert.dogan@me.com>
2021-07-23 00:58:01 +0200

    changeUsername successfull changed alert
    also colors added
    
    .

--- src/sides/usersettings/ChangeUsername.tsx
+++ src/sides/usersettings/ChangeUsername.tsx
@@ -4,7 +4,8 @@ import { changeUsername } from "../../apis/authServer/Users";
 import { GetCookie } from "../../apis/Cookies";
 
 function ChangeUsername() {
-  let [error, setError] = useState("");
+  let [alert, setAlert] = useState("");
+  let [color, setColor] = useState("255;0;0");
 
   async function onButtonClick() {
     const newUsername: HTMLInputElement = document.getElementById(
@@ -12,21 +13,27 @@ function ChangeUsername() {
     ) as HTMLInputElement;
 
     if (!newUsername.value) {
-      setError("New Username is Empty");
+      setColor("255;0;0");
+      setAlert("New Username is Empty");
+      console.log("Error");
       return;
     }
 
     try {
       await changeUsername(newUsername.value, GetCookie("jwttoken"));
+
+      setColor("179;146;0;0");
+      setAlert("Username has been changed");
     } catch (error) {
-      setError(error.name);
+      setColor("255;0;0");
+      setAlert(error.name);
     }
   }
 
   return (
     <div id="changeUsername">
       <h2 className="header">Change Username</h2>
-      <Alert id="changeUsernameAlert" color="255;0;0" alert={error} />
+      <Alert id="changeUsernameAlert" color={color} alert={alert} />
       <label className="settingsLabel">New Username</label>
       <input className="settingsInput" id="newUsername" />
       <br />

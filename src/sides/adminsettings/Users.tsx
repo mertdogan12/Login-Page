59b46c9897c103e5da4f8ee9492b9cf20ede6aa9
Mert Dogan <mert.dogan@me.com>
2021-08-04 20:25:15 +0200

    adminsettings/Users.tsx checks permission

--- src/sides/adminsettings/Users.tsx
+++ src/sides/adminsettings/Users.tsx
@@ -1,4 +1,5 @@
 import { useHistory } from "react-router-dom";
+import { useCallback, useEffect, useState } from "react";
 import { HasPermission } from "../../apis/authServer/Permission";
 import { Jwt } from "../../apis/authServer/Users";
 import { GetCookie } from "../../apis/Cookies";
@@ -6,19 +7,26 @@ import { GetCookie } from "../../apis/Cookies";
 function Users() {
   let history = useHistory();
   let token = GetCookie("jwttoken");
+  let [text, setText] = useState("Loading");
 
-  async function foo() {
+  const checkPermission = useCallback(async () => {
     try {
       if (
         !(await HasPermission((await Jwt(token)).id, "adminsettings.?", token))
       )
         history.push("/dashboard/settings/usersettings");
+
+      setText("Hallo");
     } catch (e) {
       history.push("/dashboard/settings/usersettings");
     }
-  }
+  }, [history, token]);
 
-  return <p>Loading</p>;
+  useEffect(() => {
+    checkPermission();
+  }, [checkPermission]);
+
+  return <p>{text}</p>;
 }
 
 export default Users;

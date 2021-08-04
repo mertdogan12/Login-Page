e32aa1494e46ed4105403c2032738ba63d04911c
Mert Dogan <mert.dogan@me.com>
2021-08-05 00:27:29 +0200

    adminsetting/Users.tsx update

--- src/sides/adminsettings/Users.tsx
+++ src/sides/adminsettings/Users.tsx
@@ -1,13 +1,14 @@
 import { useHistory } from "react-router-dom";
 import { useCallback, useEffect, useState } from "react";
 import { HasPermission } from "../../apis/authServer/Permission";
-import { Jwt } from "../../apis/authServer/Users";
+import { GetUsers, Jwt, User } from "../../apis/authServer/Users";
 import { GetCookie } from "../../apis/Cookies";
 
 function Users() {
   let history = useHistory();
   let token = GetCookie("jwttoken");
   let [text, setText] = useState("Loading");
+  let [users, setUsers] = useState(new Array<User>());
 
   const checkPermission = useCallback(async () => {
     try {
@@ -22,11 +23,23 @@ function Users() {
     }
   }, [history, token]);
 
+  const getUsers = useCallback(async () => {
+    setUsers(await GetUsers(token));
+  }, [token]);
+
   useEffect(() => {
     checkPermission();
-  }, [checkPermission]);
+    getUsers();
+  }, [checkPermission, getUsers]);
 
-  return <p>{text}</p>;
+  return (
+    <div>
+      <p>{text}</p>
+      {users.map((obj) => {
+        return <p key={obj.id}>{obj.name}</p>;
+      })}
+    </div>
+  );
 }
 
 export default Users;

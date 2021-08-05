d7b624ae500b4ae7600c03dc8f72f347aa402f20
Mert Dogan <mert.dogan@me.com>
2021-08-06 01:04:09 +0200

    adminsettings/Users udpated

--- src/sides/adminsettings/Users.tsx
+++ src/sides/adminsettings/Users.tsx
@@ -1,42 +1,58 @@
-import { useHistory } from "react-router-dom";
 import { useCallback, useEffect, useState } from "react";
-import { HasPermission } from "../../apis/authServer/Permission";
-import { GetUsers, Jwt, User } from "../../apis/authServer/Users";
+import { GetPermissions } from "../../apis/authServer/Permission";
+import { GetUsers, User } from "../../apis/authServer/Users";
 import { GetCookie } from "../../apis/Cookies";
 
 function Users() {
-  let history = useHistory();
   let token = GetCookie("jwttoken");
   let [text, setText] = useState(<div className="lds-dual-ring"></div>);
-  let [users, setUsers] = useState(new Array<User>());
+  let [users, setUsers] = useState(new Array<UserWithPermission>());
 
-  const checkPermission = useCallback(async () => {
-    try {
-      if (
-        !(await HasPermission((await Jwt(token)).id, "adminsettings.?", token))
-      )
-        history.push("/dashboard/settings/usersettings");
-
-      setText(<div></div>);
-    } catch (e) {
-      history.push("/dashboard/settings/usersettings");
-    }
-  }, [history, token]);
+  type UserWithPermission = {
+    id: string;
+    name: string;
+    permissions: string[];
+  };
 
   const getUsers = useCallback(async () => {
-    setUsers(await GetUsers(token));
+    try {
+      const users: User[] = await GetUsers(token);
+      const userWithPermissions: UserWithPermission[] = await Promise.all(
+        users.map(async (user: User) => {
+          return {
+            id: user.id,
+            name: user.name,
+            permissions: await GetPermissions(token, user.id),
+          };
+        })
+      );
+
+      setText(<div></div>);
+      setUsers(userWithPermissions);
+    } catch (e) {
+      console.log(e);
+      setText(<h1>No Permission</h1>);
+    }
   }, [token]);
 
   useEffect(() => {
-    checkPermission();
     getUsers();
-  }, [checkPermission, getUsers]);
+  }, [getUsers]);
 
   return (
     <div className="settingselemtent">
       {text}
       {users.map((obj) => {
-        return <p key={obj.id}>{obj.name}</p>;
+        return (
+          <details key={obj.id}>
+            <summary>{obj.name}</summary>
+            <ul>
+              {obj.permissions.map((permission, index) => {
+                return <li key={index}>{permission}</li>;
+              })}
+            </ul>
+          </details>
+        );
       })}
     </div>
   );

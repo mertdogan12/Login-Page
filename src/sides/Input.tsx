1b92d68989548b3350ff0506a151edb64baeb608
Mert Dogan <mert.dogan@me.com>
2021-08-16 03:26:28 +0200

    Input background blur added

--- src/sides/Input.tsx
+++ src/sides/Input.tsx
@@ -1,4 +1,5 @@
-import { useState, KeyboardEvent, useEffect } from "react";
+import { useState, KeyboardEvent, useEffect, useCallback } from "react";
+import { useHistory } from "react-router-dom";
 import Alert from "./Alert";
 
 export type Callback = {
@@ -14,23 +15,48 @@ type MyProps = {
 
 function Input(props: MyProps) {
   const element: HTMLElement | null = document.getElementById(props.id);
+  let history = useHistory();
   let [alert, setAlert] = useState({
     color: "",
     alert: "",
   });
+
+  const changeBackgroundFilter = useCallback(
+    (filter: string) => {
+      const htmlElement: HTMLElement = document
+        .getElementsByTagName("html")
+        .item(0) as HTMLElement;
+
+      const summaryElemets: HTMLCollection =
+        htmlElement.getElementsByTagName("details");
+
+      (document.getElementById("navbar") as HTMLElement).style.filter = filter;
+      (document.getElementById("sidebar") as HTMLElement).style.filter = filter;
+
+      for (let i = 0; i < summaryElemets.length; i++) {
+        const element: HTMLElement = summaryElemets.item(i) as HTMLElement;
+
+        if (element.id === props.id) continue;
+
+        element.style.filter = filter;
+      }
+    },
+    [props.id]
+  );
 
   useEffect(() => {
     if (element == null) return;
 
     if (props.callback) {
       element.style.display = "block";
+      changeBackgroundFilter("blur(8px)");
 
       setAlert({
         color: "255;0;0",
         alert: "",
       });
     } else element.style.display = "none";
-  }, [props.callback, element]);
+  }, [props.callback, element, props.id, changeBackgroundFilter]);
 
   async function onKeyDown(event: KeyboardEvent<HTMLInputElement>) {
     if (props.callback == null) return;
@@ -46,10 +72,15 @@ function Input(props: MyProps) {
 
         await props.callback.function(...args);
 
+        element.style.display = "none";
+        changeBackgroundFilter("none");
+
         setAlert({
           color: "179;146;0",
           alert: "No errors",
         });
+
+        setTimeout(() => window.location.reload(), 1000);
       } catch (e) {
         console.log(e);
 
@@ -58,8 +89,6 @@ function Input(props: MyProps) {
           alert: e.name,
         });
       }
-
-      element.style.display = "none";
     }
   }
 
